68fe6be8f6fa52544f1c0681ad07643841af9470
Vishakha-0904 <pvishakha719@gmail.com>
2025-07-24 18:48:58 +0530

    Update Layout.jsx

--- frontend/src/components/Layout.jsx
+++ frontend/src/components/Layout.jsx
@@ -26,7 +26,9 @@ const Layout = ({onLogout,user}) => {
         const token = localStorage.getItem('token')
         if(!token) throw new Error("No auth token found")
 
-        const {data} = await axios.get("http://localhost:4000/api/tasks/gp", {
+        // const {data} = await axios.get("http://localhost:4000/api/tasks/gp", {
+
+         const {data} = await axios.get("https://task-manager-app-vlcr.onrender.com/api/tasks/gp", {
 
             
 
